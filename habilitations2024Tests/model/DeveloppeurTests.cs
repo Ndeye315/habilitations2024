defad9e54a1ae1f8d9f2aead0039c3c5958bb244
Ndeye315 <nogayeseck04@gmail.com>
2025-06-03 10:34:20 +0200

    Update DeveloppeurTests.cs

--- habilitations2024Tests/model/DeveloppeurTests.cs
+++ habilitations2024Tests/model/DeveloppeurTests.cs
@@ -1,5 +1,31 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-
+public static List<Developpeur> GetLesDeveloppeurs(string profil = null)
+{
+    List<Developpeur> lesDeveloppeurs = new List<Developpeur>();
+    string req = "SELECT * FROM developpeur";
+    if (!string.IsNullOrEmpty(profil))
+    {
+        req += " WHERE profil = @profil";
+    }
+    SqlCommand cmd = new SqlCommand(req, ConnexionSql.Connection);
+    if (!string.IsNullOrEmpty(profil))
+    {
+        cmd.Parameters.AddWithValue("@profil", profil);
+    }
+    SqlDataReader reader = cmd.ExecuteReader();
+    while (reader.Read())
+    {
+        Developpeur d = new Developpeur(
+            int.Parse(reader["iddev"].ToString()),
+            reader["nom"].ToString(),
+            reader["prenom"].ToString(),
+            reader["profil"].ToString()
+        );
+        lesDeveloppeurs.Add(d);
+    }
+    reader.Close();
+    return lesDeveloppeurs;
+}
 namespace habilitations2024.model.Tests
 {
     [TestClass()]
